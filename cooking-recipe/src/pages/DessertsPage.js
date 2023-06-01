d99009971385e031ac9ccf66a0fd88db6b87beb0
Julie-mg <62802039+Julie-mg@users.noreply.github.com>
2023-06-01 10:41:52 +0200

    ajout page desserts

--- cooking-recipe/src/pages/DessertsPage.js
+++ cooking-recipe/src/pages/DessertsPage.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { recipeList } from '../datas/recipeList';
+import { Link } from 'react-router-dom';
+
+function renderDifficulty(difficulty) {
+  const fireSymbol = '🔥';
+  return fireSymbol.repeat(difficulty);
+}
 
 function DessertsPage() {
+  let filteredRecipes = recipeList;
+
   return (
     <div>
       <h1>Voici la page des Desserts !</h1>
+      <ul>
+      {filteredRecipes
+        .filter(recipe => recipe.category === 'dessert')
+        .map(({ name, difficulty, preparationTime, cookTime, picture }) => (
+          <div className="recipe-item" key={name}>
+              <h2>{name}</h2>
+              <p>Difficulté : {renderDifficulty(difficulty)}</p>
+              <p>Temps Total : {preparationTime + cookTime} min</p>
+			<Link to={`/recipe/${name}`}>  
+              <img src={picture} className="render" alt={name} />
+            </Link>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
