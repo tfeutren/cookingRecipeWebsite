ba66b1b1c71368a3fb206742a5be5e26fb3c4c71
ran190 <101316992+ran190@users.noreply.github.com>
2023-06-05 15:23:22 +0200

    Add in questionList
    ajouter des questions de Le saviez-vous

--- cooking-recipe/src/datas/questionList.js
+++ cooking-recipe/src/datas/questionList.js
@@ -16,6 +16,42 @@ export const questionList = [
         answer: "En terme de densité nutritionnelle (apport de nutriments pour 100 calories) certains végétaux battent largement les produits laitiers au niveau du taux de calcium : les épinards ou les brocolis apportent environ 400 mg de calcium pour 100 calories, contre 190 pour le lait! "
     },
     {
+        question: "On ne s'hydrate pas seulement en buvant de l'eau",
+        answer: "Devant la pomme, et la courgette, le concombre est l'aliment le plus hydratant : il est composé à 96% d'eau.",
+    },
+    {
+        question: "Nutella",
+        answer: "Tous les pots de Nutella vendus en une année suffiraient à recouvrir la Grande Muraille de Chine 8 fois et représentent 1,8 fois le tour du monde.",
+    },
+    {
+        question: "la nourriture que l'on consomme lorsque l'on prend l'avion possède un goût différent",
+        answer: "En réalité, ce sont nos sens qui sont perturbés. Notre perception du sucré et du salé diminue de 30% lorsque l'on voyage en avion. L'origine de ce phénomène ? La pressurisation de la cabine, qui fait gonfler nos muqueuses et bloque le passage de l'odeur et du goût. ",
+    },
+    {
+        question: "Dessaler vos plats",
+        answer: "Trop de sel : un grand classique des accidents de cuisson. Voici une petite astuce de cuisine facile : mettez 3 cuillères à soupe de riz dans un bouillon et laissez 20 minutes avant d'utiliser la passoire. Le riz absorbe le sel. Au cas où une soupe épaisse est trop salée, râpez une pomme de terre et mettez-la dedans. La crème fraîche peut être également utilisée pour dessaler une sauce ; le miel ou une soupe peuvent également servir à consommer des légumes trop salés. ",
+    },
+    {
+        question: "Râper de la mozzarella",
+        answer: "Il est normalement compliqué de râper de la mozzarella. Alors comment faire si vous vouliez en râper sur une pizza ? Tamponnez-la, mettez un film alimentaire autour et mettez-la dans votre congélateur. Après 30 minutes environ, la mozzarella peut être râpée sans problème.",
+    },
+    {
+        question: "Le plat est trop relevé",
+        answer: "Si vous avez été trop généreuse avec les épices dans votre chili, vous pouvez ajouter de la crème fraîche ou du lait pour adoucir votre plat.",
+    },
+    {
+        question: "Des pâtes trop cuites",
+        answer: "Chaque minute compte dans la cuisson de pâtes. Une minute de trop et on a gâché le plat. Que faire ? Voici une astuce de cuisine facile à réaliser : mettez les pâtes sous l'eau froide et faites-les revenir ensuite dans une grande poêle avec du beurre chaud. Ainsi, les pâtes redeviendront plus fermes.",
+    },
+    {
+        question: "Du riz qui colle",
+        answer: "Si vous voulez éviter que votre riz ne colle, mettez une tranche de pain dessus après la cuisson. Le pain absorbe l'humidité et vous obtiendrez un riz non-collant.",
+    },
+    {
+        question: "Conserver des bananes",
+        answer: "Pour les garder intactes, il suffit en effet : de ne surtout pas les détacher prématurément (on le fait juste avant d'en manger une), et d'envelopper leur queue dans un bout d'aluminium.",
+    },
+    {
         question: "Un aliment qui ne périme jamais",
         answer: "Le nectar peut contenir 70% d'eau. Les abeilles, avec un système de vent avec leurs ailes vont faire évaporer l'eau pour qu'il n'en contienne plus qu'environ 17%. C'est cette faible teneur en haut qui va rendre impossible la croissance de moisissures et bactéries ."
     }
