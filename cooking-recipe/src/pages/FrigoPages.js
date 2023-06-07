bc0afee582d7620736cc94013bf81453e6930ae7
ran190 <101316992+ran190@users.noreply.github.com>
2023-06-07 23:44:03 +0200

    Filter in Frigo
    Le filtre est seulement pour changer de majuscules en minuscules. Je n'ai pas encore fait les accents.

--- cooking-recipe/src/pages/FrigoPages.js
+++ cooking-recipe/src/pages/FrigoPages.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { recipeList } from '../datas/recipeList';
+import { Link } from 'react-router-dom';
+import { renderDifficulty } from '../utility';
 
+const RecipeList = ({ searchIngredient }) => {
+  // Filter recipes based on the specified ingredient
+  const filteredRecipes = recipeList.filter(recipe => {
+    return recipe.ingredients.some(ingredient => ingredient.name.toLowerCase().includes(searchIngredient.toLowerCase()));
+    // some: method that tests whether at least one element in the array passes the provided condition
+  });
+
+  return (
+    <div>
+      {filteredRecipes.map(recipe => (
+        <Link to="/recipe/${recipe.name}" className="link">
+        <div>
+          <h2>{recipe.name}</h2>
+          <p>Difficulté : {renderDifficulty(recipe.difficulty)}</p>
+          <p>Temps Total : {recipe.preparationTime + recipe.cookTime} min</p>
+          <img src={recipe.picture} className="render" alt={recipe.name} />
+        </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+// not used class
 class NameForm extends React.Component {
   constructor(props) {
     super(props);
@@ -25,12 +52,25 @@ class NameForm extends React.Component {
   }
 }
 
-function FrigoPage() {
+const FrigoPage = () => {
+  const [ingredient, setIngredient] = useState('');
+
+  const handleInputChange = event => {
+    setIngredient(event.target.value);
+  };
+
   return (
     <div>
       <h1>Voici la page Frigo !</h1>
+      <form>
+        <label>
+          Nom :
+          <input type="text" value={ingredient} onChange={handleInputChange} />
+        </label>
+      </form>
+      <RecipeList searchIngredient={ingredient} />
     </div>
   );
-}
+};
 
 export default FrigoPage;
