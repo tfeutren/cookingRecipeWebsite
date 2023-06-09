58402f86cd5b3245887ae2b9dcc40c1225567fb0
Julie-mg <62802039+Julie-mg@users.noreply.github.com>
2023-06-09 10:09:19 +0200

    modif FrigoPage

--- cooking-recipe/src/App.js
+++ cooking-recipe/src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
-import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+
 import AccueilPage from './pages/AccueilPage';
+import FrigoPage from './pages/FrigoPage';
+import RecipePage from './pages/RecipePage';
+import Formulaire from './pages/Formulaire';
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import { MealCategory } from "./utility"
+
+import RecipeGalery from './components/RecipeGalery';
 
 function App() {
 
-  const [filters, setFilters] = useState();
-
   return (
-    <AccueilPage key={1}/>
+    <div className='App'>
+      <Router>
+        <Banner />
+        {/* Define the link pages */}
+        <Routes>
+          <Route path="/" element={<AccueilPage />} />
+          <Route path="/entrees" element={<RecipeGalery recipeFilter={r => r.category === MealCategory.entree} />} />
+          <Route path="/plats" element={<RecipeGalery recipeFilter={r => r.category === MealCategory.plat} />} />
+          <Route path="/desserts" element={<RecipeGalery recipeFilter={r => r.category === MealCategory.dessert} />} />
+          <Route path="/frigo" element={<FrigoPage />} />
+          <Route path="/recette/:name" element={<RecipePage />} />
+          <Route path="/formulaire" element={<Formulaire />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
